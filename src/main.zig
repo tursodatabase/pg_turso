2e833616fe32d674745147c760e2afceefa8b2ce
Piotr Sarna <sarna@chiselstrike.com>
2023-06-15 11:05:29 +0200

    cleanup

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,41 @@
 const std = @import("std");
-const pg_magic = @import("pg_magic");
 const testing = std.testing;
 
 const pg = @cImport({
     @cInclude("postgres.h");
     @cInclude("replication/logical.h");
 });
+
+// Magic PostgreSQL symbols to indicate it's a loadable module
+pub const PG_MAGIC_FUNCTION_NAME = Pg_magic_func;
+pub const PG_MAGIC_FUNCTION_NAME_STRING = "Pg_magic_func";
+
+pub const PGModuleMagicFunction = ?*const fn () callconv(.C) [*c]const Pg_magic_struct;
+pub const Pg_magic_struct = extern struct {
+    len: c_int,
+    version: c_int,
+    funcmaxargs: c_int,
+    indexmaxkeys: c_int,
+    namedatalen: c_int,
+    float8byval: c_int,
+    abi_extra: [32]u8,
+};
+
+pub export fn Pg_magic_func() [*c]const Pg_magic_struct {
+    const Pg_magic_data = struct {
+        const static: Pg_magic_struct = Pg_magic_struct{
+            .len = @bitCast(c_int, @truncate(c_uint, @sizeOf(Pg_magic_struct))),
+            .version = @divTrunc(@as(c_int, 160000), @as(c_int, 100)),
+            .funcmaxargs = @as(c_int, 100),
+            .indexmaxkeys = @as(c_int, 32),
+            .namedatalen = @as(c_int, 64),
+            .float8byval = @as(c_int, 1),
+            .abi_extra = [32]u8{ 'P', 'o', 's', 't', 'g', 'r', 'e', 'S', 'Q', 'L', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+        };
+    };
+    return &Pg_magic_data.static;
+}
+// end of magic PostgreSQL symbols
 
 pub export fn _PG_output_plugin_init(arg_cb: pg.OutputPluginCallbacks) void {
     var cb = arg_cb;
