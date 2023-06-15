76efa9a526bb929f264ee3c74026999aab16f37f
Piotr Sarna <sarna@chiselstrike.com>
2023-06-15 11:15:13 +0200

    It works and loads in Postgres!!!!!!!!11

--- src/main.zig
+++ src/main.zig
@@ -25,7 +25,7 @@ pub export fn Pg_magic_func() [*c]const Pg_magic_struct {
     const Pg_magic_data = struct {
         const static: Pg_magic_struct = Pg_magic_struct{
             .len = @bitCast(c_int, @truncate(c_uint, @sizeOf(Pg_magic_struct))),
-            .version = @divTrunc(@as(c_int, 160000), @as(c_int, 100)),
+            .version = @divTrunc(@as(c_int, 150000), @as(c_int, 100)),
             .funcmaxargs = @as(c_int, 100),
             .indexmaxkeys = @as(c_int, 32),
             .namedatalen = @as(c_int, 64),
@@ -37,29 +37,30 @@ pub export fn Pg_magic_func() [*c]const Pg_magic_struct {
 }
 // end of magic PostgreSQL symbols
 
-pub export fn _PG_output_plugin_init(arg_cb: pg.OutputPluginCallbacks) void {
+pub export fn _PG_output_plugin_init(arg_cb: [*c]pg.OutputPluginCallbacks) void {
+    std.debug.print("Initializing pgturso plugin!!!!!!!!111\n", .{});
     var cb = arg_cb;
-    cb.startup_cb = &pg_decode_startup;
-    cb.begin_cb = &pg_decode_begin_txn;
-    cb.change_cb = &pg_decode_change;
-    cb.truncate_cb = &pg_decode_truncate;
-    cb.commit_cb = &pg_decode_commit_txn;
-    cb.filter_by_origin_cb = &pg_decode_filter;
-    cb.shutdown_cb = &pg_decode_shutdown;
-    cb.message_cb = &pg_decode_message;
-    cb.filter_prepare_cb = &pg_decode_filter_prepare;
-    cb.begin_prepare_cb = &pg_decode_begin_prepare_txn;
-    cb.prepare_cb = &pg_decode_prepare_txn;
-    cb.commit_prepared_cb = &pg_decode_commit_prepared_txn;
-    cb.rollback_prepared_cb = &pg_decode_rollback_prepared_txn;
-    cb.stream_start_cb = &pg_decode_stream_start;
-    cb.stream_stop_cb = &pg_decode_stream_stop;
-    cb.stream_abort_cb = &pg_decode_stream_abort;
-    cb.stream_prepare_cb = &pg_decode_stream_prepare;
-    cb.stream_commit_cb = &pg_decode_stream_commit;
-    cb.stream_change_cb = &pg_decode_stream_change;
-    cb.stream_message_cb = &pg_decode_stream_message;
-    cb.stream_truncate_cb = &pg_decode_stream_truncate;
+    cb.*.startup_cb = &pg_decode_startup;
+    cb.*.begin_cb = &pg_decode_begin_txn;
+    cb.*.change_cb = &pg_decode_change;
+    cb.*.truncate_cb = &pg_decode_truncate;
+    cb.*.commit_cb = &pg_decode_commit_txn;
+    cb.*.filter_by_origin_cb = &pg_decode_filter;
+    cb.*.shutdown_cb = &pg_decode_shutdown;
+    cb.*.message_cb = &pg_decode_message;
+    cb.*.filter_prepare_cb = &pg_decode_filter_prepare;
+    cb.*.begin_prepare_cb = &pg_decode_begin_prepare_txn;
+    cb.*.prepare_cb = &pg_decode_prepare_txn;
+    cb.*.commit_prepared_cb = &pg_decode_commit_prepared_txn;
+    cb.*.rollback_prepared_cb = &pg_decode_rollback_prepared_txn;
+    cb.*.stream_start_cb = &pg_decode_stream_start;
+    cb.*.stream_stop_cb = &pg_decode_stream_stop;
+    cb.*.stream_abort_cb = &pg_decode_stream_abort;
+    cb.*.stream_prepare_cb = &pg_decode_stream_prepare;
+    cb.*.stream_commit_cb = &pg_decode_stream_commit;
+    cb.*.stream_change_cb = &pg_decode_stream_change;
+    cb.*.stream_message_cb = &pg_decode_stream_message;
+    cb.*.stream_truncate_cb = &pg_decode_stream_truncate;
 }
 
 pub fn pg_decode_startup(arg_ctx: [*c]pg.LogicalDecodingContext, arg_opt: [*c]pg.OutputPluginOptions, arg_is_init: bool) callconv(.C) void {
